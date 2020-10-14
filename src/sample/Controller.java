91ef87b665893f85bbc3412d6cddaf9f9fd4c411
idanso <68230307+idanso@users.noreply.github.com>
2020-10-14 19:04:13 +0300

    added edit referee and stadium and upgrade of edit athlete abd tornament, and added a function in the enum

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -14,18 +14,14 @@ import java.util.ResourceBundle;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
 public class Controller implements Initializable {
 
     @FXML
     private Button changeTomanagerPage;
-
-    @FXML
-    private Button submitBtn;
-
-    @FXML
-    private Button deleteBtn;
 
     @FXML
     private TabPane tapPaneSummery;
@@ -37,7 +33,7 @@ public class Controller implements Initializable {
     private MenuButton tournamentMenu;
 
     @FXML
-    private TableView<?> tournamentsTableView;
+    private TableView<?> rournamentsTableView;
 
     @FXML
     private TableColumn<?, ?> tournamentTypeCol;
@@ -58,12 +54,6 @@ public class Controller implements Initializable {
     private MenuButton countryMenu;
 
     @FXML
-    private TextField countryInput;
-
-    @FXML
-    private ChoiceBox sportTypeBox;
-
-    @FXML
     private TableView<?> athletesTableView;
 
     @FXML
@@ -76,7 +66,28 @@ public class Controller implements Initializable {
     private TableColumn<?, ?> athleteTypecol;
 
     @FXML
-    private Tab viewRefereesTab;
+    private Tab viewMedalsTab;
+
+    @FXML
+    private Label countryFirstLable;
+
+    @FXML
+    private Label numOfMedalsFirstLable;
+
+    @FXML
+    private Label countrySecondLable;
+
+    @FXML
+    private Label numOfMedalsSecondLable;
+
+    @FXML
+    private Label countryThirdLable;
+
+    @FXML
+    private Label numOfMedalsThirdLable;
+
+    @FXML
+    private Button athleteSubmitBtn;
 
     @FXML
     private TextField fullNameRegisterInput;
@@ -85,25 +96,71 @@ public class Controller implements Initializable {
     private TextField fullNameDeleteInput;
 
     @FXML
-    private TableView<?> refereeTableView;
+    private Button athleteDeleteBtn;
 
     @FXML
-    private TableColumn<?, ?> refereeNameCol;
+    private ChoiceBox<SportTypeAthleteANDReferee> sportTypeBox;
 
     @FXML
-    private TableColumn<?, ?> refereeTypeCol;
+    private TextField countryInput;
 
     @FXML
-    private Tab viewMedalsTab;
+    private ChoiceBox<String> tournamentType;
 
     @FXML
-    private TableView<?> medalsTableView;
+    private ChoiceBox<String> sportTypeBox1;
 
     @FXML
-    private TableColumn<?, ?> medalCountryNameCol;
+    private ChoiceBox<String> refereeBox;
 
     @FXML
-    private TableColumn<?, ?> medalAllMedalsCol;
+    private ChoiceBox<String> stadiumBox;
+
+    @FXML
+    private ChoiceBox<String> countryBox;
+
+    @FXML
+    private ChoiceBox<String> countryBox1;
+
+    @FXML
+    private ChoiceBox<String> athleteBox1;
+
+    @FXML
+    private ChoiceBox<String> athleteBox2;
+
+    @FXML
+    private Button createTornamentBtn;
+
+    @FXML
+    private TextField stadiumNameInput;
+
+    @FXML
+    private TextField stadiumLocationInput;
+
+    @FXML
+    private TextField stadiumSeatsInput;
+
+    @FXML
+    private Button addStadiumBtn;
+
+    @FXML
+    private Label seatsInputExceptionLabel;
+
+    @FXML
+    private Button refereeSubmitBtn;
+
+    @FXML
+    private TextField refereeFullNameInput;
+
+    @FXML
+    private TextField refereeFullNameInput1;
+
+    @FXML
+    private ChoiceBox<SportTypeAthleteANDReferee> sportTypeBox2;
+
+    @FXML
+    private TextField refereeCountryInput;
+
 
     /*public void sceneSwitchEvent (ActionEvent event) throws IOException {
         Parent root;
@@ -118,32 +175,38 @@ public class Controller implements Initializable {
     public void menuButtonEvent (String tornamentType){
         //  tournamentTypeCol.setCellValueFactory(new PropertyValueFactory<>());
     }
-    public void buttonEvent (ActionEvent e) {
+    public void editAthleteBtnsEvent (ActionEvent e) {
         String fullName;
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        boolean actionStatus = true;//to change if needed
+        boolean actionStatus = false;//to change if needed
         alert.setTitle("status massage");
         try {
-            if (e.getSource() == submitBtn) {
+            if (e.getSource() == athleteSubmitBtn) {
                 fullName = fullNameRegisterInput.getText();
                 SportTypeAthleteANDReferee sportType = (SportTypeAthleteANDReferee)sportTypeBox.getValue();
-                System.out.println(sportType);
                 String country = countryInput.getText();
-                // boolean = (retunr if player added or not) need to send to th add athlete function
-                if (actionStatus)
-                    alert.setHeaderText("Athlete added sucesfully!");
-                else
-                    alert.setHeaderText("somthing went wrong, athlete didn't added");
-                alert.showAndWait();
-
-            } else if (e.getSource() == deleteBtn) {
+                if (fullName != null && sportType != null && country != null) {
+                    //**********send to delete player function*************
+                    // boolean = (retunr if player added or not) need to send to th add athlete function
+                    if (actionStatus)
+                        alert.setHeaderText("Athlete added successfully!");
+                    else
+                        alert.setHeaderText("something went wrong, athlete didn't added");
+                    alert.showAndWait();
+                } else
+                    emptyFieldsDialog();
+            } else if (e.getSource() == athleteDeleteBtn) {
                 fullName = fullNameDeleteInput.getText();
-                //send to delete player function
-                if (actionStatus)
-                    alert.setHeaderText("Athlete delete sucesfully!");
+                if (fullNameDeleteInput != null) {
+                    //**********send to delete player function*************
+                    if (actionStatus)
+                        alert.setHeaderText("Athlete delete sucesfully!");
+                    else
+                        alert.setHeaderText("somthing went wrong, athlete didn't delete");
+                    alert.showAndWait();
+                }
                 else
-                    alert.setHeaderText("somthing went wrong, athlete didn't delete");
-                alert.showAndWait();
+                    emptyFieldsDialog();
             }
         } catch (Exception e1){
             alert.setHeaderText("somthing went wrong, try again and fill all " +
@@ -152,6 +215,63 @@ public class Controller implements Initializable {
         }
     }
 
+    public void addStadiumBtn (ActionEvent e){
+        seatsInputExceptionLabel.setText("");
+        String name, location;
+        int numOfSeats = 0;
+        name = stadiumNameInput.getText();
+        location = stadiumLocationInput.getText();
+
+        try {
+            numOfSeats = Integer.valueOf(stadiumSeatsInput.getText());
+            if (numOfSeats == 0)
+                throw new Exception("the number zero not allowed");
+        } catch(Exception exception) {
+            seatsInputExceptionLabel.setTextFill(Paint.valueOf("Red"));
+            seatsInputExceptionLabel.setText("enter only numbers and no zero");
+            stadiumSeatsInput.clear();
+
+        }
+        if (name != null && location != null && numOfSeats != 0){
+            //************to fill function to add stadium to system*************
+            stadiumSeatsInput.clear();
+            stadiumLocationInput.clear();
+            stadiumNameInput.clear();
+        }
+        else {
+            stadiumSeatsInput.clear();
+            stadiumLocationInput.clear();
+            stadiumNameInput.clear();
+            emptyFieldsDialog();
+        }
+
+    }
+
+    public void tornamenBoxFill (ActionEvent e){
+        String strTournamentType = tournamentType.getValue();
+        String sportType = sportTypeBox1.getValue();
+        if(sportType != null && strTournamentType != null){
+            refereeBox.getItems().clear();
+            refereeBox.getItems().addAll();//need to add the array of all matching referees
+            if (countryBox != null){
+                athleteBox1.getItems().addAll();////need to add the array of all matching referees
+            }
+            if (countryBox1 != null){
+                athleteBox2.getItems().addAll();////need to add the array of all matching referees
+            }
+        }
+    }
+    public void addRefereeBtn (ActionEvent e){
+        String name = refereeFullNameInput1.getText();
+        String country = refereeCountryInput.getText();
+        SportTypeAthleteANDReferee sportType = sportTypeBox2.getValue();
+        if (name != null && country != null && sportType != null ){
+
+        }
+        else{
+
+        }
+    }
 
 
 
@@ -173,16 +293,25 @@ public class Controller implements Initializable {
         }
         tournamentMenu.getItems().addAll(menulistArray);
 
-        sportTypeBox.getItems().addAll(SportTypeAthleteANDReferee.values());
-
-
         for (int i = 0; i < 5; i++) {
             menulistArray.add(new MenuItem("country num " + (i+1)));//need to add refference to all countrys array
         }
         countryMenu.getItems().addAll(menulistArray);
+        tournamentType.getItems().addAll("solo", "group");
+        sportTypeBox.getItems().addAll(SportTypeAthleteANDReferee.values());
+        sportTypeBox1.getItems().addAll(SportTypeAthleteANDReferee.getAllSportTypes());
+        sportTypeBox2.getItems().addAll(SportTypeAthleteANDReferee.values());
+        stadiumBox.getItems().addAll();//need to add the array of all stadiums
+        countryBox.getItems().addAll();//need to add the array of all countrys
+        countryBox1.getItems().addAll();//need to add the array of all countrys
 
+    }
 
-
+    public void emptyFieldsDialog (){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("status massage");
+        alert.setHeaderText("please fill all the fields correctly");
+        alert.showAndWait();
     }
 
 }
--- src/sample/SportTypeAthleteANDReferee.java
+++ src/sample/SportTypeAthleteANDReferee.java
@@ -3,5 +3,16 @@ package sample;
 public enum SportTypeAthleteANDReferee {
         Runner,
         Jumper,
-        RunnerANDJumper
+        RunnerAndJumper;
+
+        public static String[] getAllSportTypes() {
+
+                SportTypeAthleteANDReferee[] eArray = SportTypeAthleteANDReferee.values();
+                String[] str = new String[SportTypeAthleteANDReferee.values().length - 1];
+                for (int i = 0; i < SportTypeAthleteANDReferee.values().length - 1; i++) {
+                        str[i] = eArray[i].toString();
+                }
+
+                return str;
+        }
 }
